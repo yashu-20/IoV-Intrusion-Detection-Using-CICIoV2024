f9b46143096447412653ecc678e6b821243a170c
yash <yashu.kovvuri@gmail.com>
2025-05-21 20:37:56 -0500

    fix: Use 'Agg' backend for matplotlib in CI

--- Original_data.py
+++ Original_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, StratifiedKFold, learning_curve
